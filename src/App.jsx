9620b8e43a18aae6dbaec4f01609a64d566d6dd5
Will Baxter <bax.will@googlemail.com>
2024-04-24 15:48:15 +0100

    Rendered emails

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import Header from './components/Header'
 import initialEmails from './data/emails'
-
+import { useState } from 'react'
 import './styles/App.css'
 
 function App() {
-  // Use initialEmails for state
-  console.log(initialEmails)
+
+  const [emails, updateEmails] = useState(initialEmails)
+ 
+  function toggleRead(email) {
+    email.read = !email.read
+    updateEmails([...emails])
+  }
+  
+  function toggleStar(email) {
+    email.starred = !email.starred
+    updateEmails([...emails])
+    console.log(emails)
+  }
 
   return (
     <div className="app">
@@ -28,7 +39,7 @@ function App() {
           </li>
 
           <li className="item toggle">
-            <label for="hide-read">Hide read</label>
+            <label htmlFor="hide-read">Hide read</label>
             <input
               id="hide-read"
               type="checkbox"
@@ -38,7 +49,29 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">{ emails.map((email, index) => { return (
+           <li className="email" key={index}>
+           <div className="select">
+           <input
+             onChange={() => toggleRead(email)}
+             className="select-checkbox"
+             type="checkbox"
+             checked={email.read}
+             />
+           </div>
+           <div className="star">
+           <input
+             onChange={() => toggleStar(email)}
+             className="star-checkbox"
+             type="checkbox"
+             checked={email.starred}
+           />
+           </div>
+           <div className={email.sender}>{email.sender}</div>
+           <div className={email.title}>{email.title}</div>
+         </li>
+      ) 
+      })}</main>
     </div>
   )
 }
